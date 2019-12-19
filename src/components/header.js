cd31523897e1029a98b951b9baf57510ca209b8e
Maleeha <malehahasan@gmail.com>
2019-12-19 16:47:33 -0500

    EOD at school

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,7 @@ function Header(props) {
 
       <Link to="/" ><h1><span1>MY</span1><span2>FITNESS</span2><span3>TRACKER</span3></h1></Link>
       {!props.isLoggedOut && <Link to="/" className="logout-button" onClick={props.logoutClick}>LogOut</Link>}
+      {!props.isLoggedOut && <Link to="/home" className="back-button"  >Back</Link>}
 
 
 
